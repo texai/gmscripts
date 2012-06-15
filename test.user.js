c18910c58d760d842ad988404cafb8f701a05368
Ernesto Anaya <ernesto.anaya@ec.pe>
2012-06-15 16:50:51 -0500

    test

--- test.user.js
+++ test.user.js
@@ -22,6 +22,24 @@ function getUrlVars()
     return vars;
 }
 
+function utf8_decode (str_data) {
+    i = 0,ac = 0,c1 = 0,c2 = 0,c3 = 0;str_data += '';
+    while (i < str_data.length) {        c1 = str_data.charCodeAt(i);
+        if (c1 < 128) {
+            tmp_arr[ac++] = String.fromCharCode(c1);
+            i++;
+        } else if (c1 > 191 && c1 < 224) {            c2 = str_data.charCodeAt(i + 1);
+            tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
+            i += 2;
+        } else {
+            c2 = str_data.charCodeAt(i + 1);            c3 = str_data.charCodeAt(i + 2);
+            tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+    } 
+    return tmp_arr.join('');
+}
+
 function base64_decode (data) {
     var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
     var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
