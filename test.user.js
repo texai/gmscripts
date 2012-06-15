545b0505fed1013c4f1f1c3b065bfd91538fa65a
Ernesto Anaya <ernesto.anaya@ec.pe>
2012-06-15 16:24:13 -0500

    test

--- test.user.js
+++ test.user.js
@@ -6,11 +6,28 @@
 // @include       http://redmine.ec.pe/projects/*/issues/new*
 // ==/UserScript==
 
-alert('test2');
+alert('test3');
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
 
 $(function(){
     $('#q').click(function(){
         $('#issue_descr_fields').slideUp(2000);;
     });
+    console.log('dsa');
+    console.log({wdw:'ass'});
+    console.log(getUrlVars());
 });
 
