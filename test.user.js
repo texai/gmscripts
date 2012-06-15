9ccf994e0963910915126cd3912aaa3b17da3b41
Ernesto Anaya <ernesto.anaya@ec.pe>
2012-06-15 14:57:14 -0500

    test

--- test.user.js
+++ test.user.js
@@ -2,7 +2,12 @@
 // @name          My Test Script
 // @namespace     Test
 // @description   Scripting test
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js
 // @include       http://redmine.ec.pe/projects/aptitus/issues/new
 // ==/UserScript==
 
 alert('test1');
+
+$('#q').click(function(){
+    $('issue_descr_fields').slideUp(2000);;
+});
