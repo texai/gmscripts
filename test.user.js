c05ec824c7df2ccadf0b61b835021d1a4342ec68
Ernesto Anaya <ernesto.anaya@ec.pe>
2012-06-15 14:42:25 -0500

    test

---
+++ test.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          My Test Script
+// @namespace     Test
+// @description   Scripting test
+// @include       http://redmine.ec.pe/projects/aptitus/issues/new
+// ==/UserScript==
+
+alert('test1');
